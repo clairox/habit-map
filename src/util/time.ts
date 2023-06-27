bd31c82cd9050d1c898748dc1813a763ab034334
Claire Q <claireq620@gmail.com>
2023-06-27 18:55:09 -0400

    updated time.ts

--- src/util/time.ts
+++ src/util/time.ts
@@ -1,8 +1,12 @@
 const HOUR = 1000 * 60 * 60;
 const DAY = HOUR * 24;
 
+const normalizeDate = (date: Date): Date => {
+	return new Date(Math.floor(Date.parse(date.toString()) / DAY) * DAY);
+};
+
 export const getToday = (): Date => {
-	return new Date(Math.floor(Date.parse(new Date().toString()) / DAY) * DAY + HOUR * 4);
+	return normalizeDate(new Date());
 };
 
 export const nDaysAgo = (n: number): Date => {
@@ -13,8 +17,17 @@ export const nDaysAgo = (n: number): Date => {
 };
 
 export const nDaysBefore = (date: Date, n: number): Date => {
-	const nDaysBefore = new Date(date);
+	const nDaysBefore = normalizeDate(new Date(date));
 	nDaysBefore.setDate(nDaysBefore.getDate() - n);
 
 	return nDaysBefore;
+};
+
+export const daysUntil = (date: Date): number => {
+	const today = getToday();
+	const targetDate = normalizeDate(date);
+
+	const days = (Date.parse(targetDate.toString()) - Date.parse(today.toString())) / DAY;
+
+	return days;
 };
