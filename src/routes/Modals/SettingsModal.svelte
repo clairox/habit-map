c0b43c2cc0ba99e5003e87dfbd416ce1505454e6
Claire Q <claireq620@gmail.com>
2023-07-18 14:51:18 -0400

    created SettingsModal.svelte

---
+++ src/routes/Modals/SettingsModal.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import Modal from '../Modal.svelte';
+
+	export let showModal = false;
+
+	let dialog: HTMLDialogElement;
+
+	let onClose = () => {};
+
+	const onSubmit = () => {};
+</script>
+
+<Modal bind:showModal bind:dialog bind:onClose>
+	<h2 class="title" slot="header">Settings</h2>
+	<form slot="content" class="form" tabindex="-1" on:submit|preventDefault={onSubmit}>
+		<div class="name" />
+		<div class="theme-color" />
+		<div class="delete-data" />
+		<div class="form-actions">
+			<button class="button" type="submit">Save Changes</button>
+			<button class="button" type="button" on:click|preventDefault={() => dialog.close()}
+				>Cancel</button
+			>
+		</div>
+	</form>
+</Modal>
