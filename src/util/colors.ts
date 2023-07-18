6de3d6e9bb206d38599bd80291cb50995cdb6a17
clairox <claireq620@gmail.com>
2023-07-18 15:58:44 -0400

    updated colors.ts

--- src/util/colors.ts
+++ src/util/colors.ts
@@ -2,49 +2,46 @@ export const colors = [
 	{
 		name: 'Coral',
 		primaryColor: 'rgb(248, 99, 94)',
-		lightColor: 'rgb(245, 143, 140)',
-		darkColor: 'rgb(170, 74, 68)'
+		lightColor: 'rgb(245, 143, 140)'
 	},
 	{
 		name: 'Rust',
 		primaryColor: 'rgb(226, 110, 75)',
-		lightColor: 'rgb(237, 144, 116)',
-		darkColor: 'rgb(170, 74, 68)'
+		lightColor: 'rgb(237, 144, 116)'
 	},
 	{
 		name: 'Mustard',
 		primaryColor: 'rgb(240, 188, 60)',
-		lightColor: 'rgb(247, 209, 139)',
-		darkColor: 'rgb(170, 74, 68)'
+		lightColor: 'rgb(247, 209, 139)'
 	},
 	{
 		name: 'Cactus',
 		primaryColor: 'rgb(96, 160, 82)',
-		lightColor: 'rgb(152, 201, 125)',
-		darkColor: 'rgb(170, 74, 68)'
+		lightColor: 'rgb(152, 201, 125)'
 	},
 	{
 		name: 'Jade',
 		primaryColor: 'rgb(42, 170, 138)',
-		lightColor: 'rgb(85, 207, 177)',
-		darkColor: 'rgb(43, 136, 113)'
+		lightColor: 'rgb(85, 207, 177)'
 	},
 	{
 		name: 'Ocean',
 		primaryColor: 'rgb(59, 113, 159)',
-		lightColor: 'rgb(94, 161, 196)',
-		darkColor: 'rgb(43, 136, 113)'
+		lightColor: 'rgb(94, 161, 196)'
 	},
 	{
 		name: 'Aubergine',
 		primaryColor: 'rgb(128, 91, 135)',
-		lightColor: 'rgb(168, 141, 186)',
-		darkColor: 'rgb(43, 136, 113)'
+		lightColor: 'rgb(168, 141, 186)'
 	},
 	{
 		name: 'Salmon',
 		primaryColor: 'rgb(244, 191, 201)',
-		lightColor: 'rgb(255, 217, 226)',
-		darkColor: 'rgb(43, 136, 113)'
+		lightColor: 'rgb(255, 217, 226)'
+	},
+	{
+		name: 'Gray',
+		primaryColor: 'rgb(127, 130, 138)',
+		lightColor: 'rgb(183, 187, 199)'
 	}
 ];
