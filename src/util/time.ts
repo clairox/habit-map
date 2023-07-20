8f61c5cbef505cc3fc9a389aeaad706e63359b24
Claire Q <claireq620@gmail.com>
2023-07-19 22:20:48 -0400

    fixed timezone offset bug

--- src/util/time.ts
+++ src/util/time.ts
@@ -1,14 +1,22 @@
+import { browser } from '$app/environment';
+
 const MINUTE = 60 * 1000;
 const HOUR = 60 * MINUTE;
 const DAY = HOUR * 24;
 
 const normalizeDate = (date: Date): Date => {
-	const timezoneOffset = MINUTE * date.getTimezoneOffset();
-	return new Date(Math.floor(Date.parse(date.toString()) / DAY) * DAY + timezoneOffset);
+	const normalizedDate = date;
+
+	normalizedDate.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+	return new Date(
+		Math.floor(Date.parse(normalizedDate.toString()) / DAY) * DAY +
+			MINUTE * date.getTimezoneOffset()
+	);
 };
 
 export const getToday = (): Date => {
-	return normalizeDate(new Date());
+	const date = new Date();
+	return normalizeDate(date);
 };
 
 export const nDaysAgo = (n: number): Date => {
